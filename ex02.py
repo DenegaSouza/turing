1b0fab0592f2988b8e4fb9c7f2ea70d852562556
Joao Victor De Oliveira <joao.oliveira@proway.treina>
2022-09-13 20:44:22 -0300

    Exercício 02 feito

---
+++ ex02.py
@@ -0,0 +1,15 @@
+"""
+Crie uma variável recebendo o conceito de polimorfismo com o sinal de igual.
+Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string cabecalho adicione quebra de linha, ao final.
+Imprima no terminal o índice de repetição.
+Crie um laco de repeticao para imprimir a função print com a palavra FELIZ DIA DO PROGRAMADOR. a frase deve ser impressa 10 vezes.
+Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois do string utilize como rodapé."""
+
+poli = "="*15
+
+print(f"\n{poli} Cabeçalho {poli}\n")
+
+for x in range (0,10):
+    print(f"{x} - FELIZ DIA DO PROGRAMADOR.")
+
+print(f"\n{poli} Rodapé {poli}\n")
