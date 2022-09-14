99883248fd61113c19f62364457c487ab453d0eb
Larissa Hoffmann Sebold <larissa.sebold@proway.treina>
2022-09-13 21:44:51 -0300

    Exercício 4 corrigido

--- ex04.py
+++ ex04.py
@@ -1,8 +1,13 @@
-
-#8º - Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string cabeçalho início estrutura de decisão adicione quebra de linha, 
-# ao final.
-#9º - Crie uma estrutura de decisão falando se a soma desses números é maior que 10, menor que 10, igual a dez, diferente de 10.
-#10º - Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string utilize como rodapé, definindo o fim da estrutura de decisão.
+"""1º - Crie uma variável com valor fixo esta variável deve conter um numero que não interfira na decisão de um índice;
+2º - Crie uma variável recebendo o conceito de polimorfismo com o sinal de igual.
+3º - Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string cabecalho adicione quebra de linha, ao final.
+4º - Crie um laco de repeticao, este laço deve solicitar ao usuário para digitar um número por 3 vezes..
+5º - Dentro do laço, crie a capacidade de soma entre esses números digitados, eles devem ser atributos de soma.
+6º - Crie uma função de impressão usando máscara de substituição e imprima de forma descritiva a soma desses números.
+7º - Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string utilize como rodapé, definindo o fim do laço de repetição.
+8º - Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string cabeçalho início estrutura de decisão adicione quebra de linha, ao final.
+9º - Crie uma estrutura de decisão falando se a soma desses números é maior que 10, menor que 10, igual a dez, diferente de 10.
+10º - Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string utilize como rodapé, definindo o fim da estrutura de decisão."""
 
 variavel1 = 0
 
