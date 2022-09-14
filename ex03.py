3d48392e0515585f6be26f422240ac8ff02816b4
Haiko Rudiger <haiko.rudiger@proway.treina>
2022-09-13 21:38:03 -0300

    exercicio 3 concluido

---
+++ ex03.py
@@ -0,0 +1,12 @@
+variavel = int(input("\nDigite um número: "))
+titulo = "="*15 
+rodape = "="*15
+
+print(f"\n{titulo}\n CABEÇALHO \n{titulo}\n")
+
+
+for x in range(0, variavel + 1):
+    print(x)
+
+
+print(f"\n{rodape}\n RODAPÉ \n{rodape}")
