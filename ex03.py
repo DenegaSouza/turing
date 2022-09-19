d7084c8bdc5a717bc12e0872e7cbdd0785646c2a
Lucas Roberto De Oliveira <lucas.oliveira@proway.treina>
2022-09-19 18:24:53 -0300

    ex03

--- ex03.py
+++ ex03.py
@@ -1,3 +1,9 @@
+"""Crie uma variável do tipo inteiro solicitando dados.
+Crie uma variável recebendo o conceito de polimorfismo com o sinal de igual.
+Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string cabecalho adicione quebra de linha, ao final.
+Crie um laco de repeticao que execute o índice de contagem crescente até o número digitado pelo usuário
+Crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois do string utilize como rodapé."""
+
 variavel = int(input("\nDigite um número: "))
 titulo = "="*15 
 rodape = "="*15
